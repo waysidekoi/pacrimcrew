328b88117275a464039e3432bc0ab779a03fde8d
Wayne Yang <waysidekoi@gmail.com>
2017-01-16 20:54:21 -0500

    Add whitespace between data and totals

--- lib/pacrimcrew/google_sheets.rb
+++ lib/pacrimcrew/google_sheets.rb
@@ -31,15 +31,15 @@ class Pacrimcrew::GoogleSheets
       row += 1
     end
 
-    sheet[row,3] = "TOTAL"
-    sheet[row,4] = "=SUM(D3:D#{stats.count})"
-    sheet[row,5] = "=SUM(E3:E#{stats.count})"
-    sheet[row,6] = "=SUM(F3:F#{stats.count})"
-    sheet[row,7] = "=SUM(G3:G#{stats.count})"
-    sheet[row,8] = "=SUM(H3:H#{stats.count})"
-    sheet[row,9] = "=SUM(I3:I#{stats.count})"
-    sheet[row,10] = "=SUM(J3:J#{stats.count})"
-    sheet[row,11] = "=SUM(K3:K#{stats.count})"
+    sheet[row+1,3] = "TOTAL"
+    sheet[row+1,4] = "=SUM(D3:D#{stats.count})"
+    sheet[row+1,5] = "=SUM(E3:E#{stats.count})"
+    sheet[row+1,6] = "=SUM(F3:F#{stats.count})"
+    sheet[row+1,7] = "=SUM(G3:G#{stats.count})"
+    sheet[row+1,8] = "=SUM(H3:H#{stats.count})"
+    sheet[row+1,9] = "=SUM(I3:I#{stats.count})"
+    sheet[row+1,10] = "=SUM(J3:J#{stats.count})"
+    sheet[row+1,11] = "=SUM(K3:K#{stats.count})"
 
     sheet.save
   end
